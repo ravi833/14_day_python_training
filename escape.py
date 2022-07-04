efd6d768b58ded9c8ff257f232e8b4f5d0a239f6
ravi833 <107167422+ravi833@users.noreply.github.com>
2022-07-04 19:55:49 +0530

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,68 @@
+'''escape sequences'''
+# print('hii\ajay')   #hiijay
+# print('hii\babu')   #hiabu
+# print('hii\\babu')  # hii\babu
+# print('hii\chanti')  #hii\chanti
+# print('hii\divya')  #hii\divya
+# print('hii\eswar')  #hii\eswar
+# print('hii\farooq')  # hii
+                     # arooq
+
+# print('hii\gowtham')  # hii\gowtham
+# print('hii\hameed')  #hii\hameed
+# print('hii\ishwarya')   #hii\ishwarya
+# print('hii\jagadesh')  #hii\jagadesh
+# print('hii\kumari')  #hii\kumari
+# print('hii\lavanya') #hii\lavanya
+# print('hii\manohar')  #hii\manohar
+# print('hii\niharika')  #hii
+                          #iharika
+# print('hii\omin')  #hii\omin
+# print('hii\pravalika')   #hii\pravalika
+# print('hii\querry')  #hii\querry
+# print('hii\ravi')  #avi
+# print('hello\ravi')  #avilo
+# print('hii\ravikiran')  #avikiran
+# print('hii\sai')  #hii\sai
+# print('hii\tharun')  #hii     harun
+# print('hii\umar')  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-4: truncated \uXXXX escape
+# print('hii\vasu')  #hii
+                      #asu
+# print('hii\warner')  #hii\warner
+# print('hii\xavier')  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-5: truncated \xXX escape
+# print('hii\yamini')  #hii\yamini
+# print('hii\zikriya')  #hii\zikriya
+
+
+
+
+# print('hii\Ajay')   #hii\Ajay
+# print('hii\Babu')   #hii\Babu
+# print('hii\\Babu')  # hii\\Babu
+# print('hii\Chanti')  #hii\Chanti
+# print('hii\Divya')  #hii\Divya
+# print('hii\Eswar')  #hii\Eswar
+# print('hii\Farooq')  # hii\Farooq
+# print('hii\Gowtham')  # hii\Gowtham
+# print('hii\Hameed')  #hii\Hameed
+# print('hii\Ishwarya')   #hii\Ishwarya
+# print('hii\Jagadesh')  #hii\Jagadesh
+# print('hii\Kumari')  #hii\Kumari
+# print('hii\Lavanya') #hii\Lavanya
+# print('hii\Manohar')  #hii\Manohar
+# print('hii\NIiharika')  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-4: malformed \N character escape
+                          
+# print('hii\Omin')  #hii\Omin
+# print('hii\Pravalika')   #hii\Pravalika
+# print('hii\Querry')  #hii\Querry
+# print('hii\Ravi')  #hii\Ravi
+
+# print('hii\Sai')  #hii\Sai
+# print('hii\Tharun')  #hii\Tharun
+# print('hii\Umar')  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-4: truncated \uXXXX escape
+# print('hii\Vasu')  #hii\Vasu
+                      
+# print('hii\Warner')  #hii\Warner
+# print('hii\Xavier')  #hii\Xavier
+# print('hii\Yamini')  #hii\Yamini
+print('hii\Zikriya')  #hii\Zikriya  
